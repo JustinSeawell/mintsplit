3114aae6c142bd5f175e21525aa3ef70cdfff222
JustinSeawell <jpseawell@gmail.com>
2022-05-09 22:15:53 -0500

    fix: update sentry project name

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,14 +2,17 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://1f9c9d38c9e947c4b304f7a845be48cf@o1232454.ingest.sentry.io/6380452',
+  dsn:
+    SENTRY_DSN ||
+    "https://1f9c9d38c9e947c4b304f7a845be48cf@o1232454.ingest.sentry.io/6380452",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
